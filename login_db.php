c6692184e84c16e58d944d871b3548d2cbfb5bcc
Penneungk <pkrit443@gmail.com>
2021-02-05 20:46:21 +0700

    Update login_db.php

--- login_db.php
+++ login_db.php
@@ -3,7 +3,7 @@
     $userId = $_POST['userId'];
     $userName = $_POST['userName'];
     $pwd = $_POST['pwd'];
-  
+  // just mock-up loging information
   if($userId == 0 && $userName == "" && $pwd == "") {
 
     $_SESSION['adminid'] = 32;
@@ -13,7 +13,7 @@
     }
     header('Location: index.php');
   }
-
+    // just mock-up loging information
   else if($userId !=  0 || $userName != "" || $pwd != ""){
 
     $_SESSION['loginfail'] = "ล็อคอินไม่สำเร็จ ใส่รหัสไม่ถูกต้อง";
